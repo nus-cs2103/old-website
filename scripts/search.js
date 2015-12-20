a2e763e58b6d0e73107783d2485f39cb6bb1ac03
Stefano Chiesa <base278@gmail.com>
2015-12-20 17:49:56 +0800

    Add build keyword tree

--- scripts/search.js
+++ scripts/search.js
@@ -1,11 +1,33 @@
 
-function fetchSearchData() {
+
+
+function fetchSearchData(callback) {
   $.getJSON('../contents/search-data.json', function(data) {
-    
+    callback(data);
+  });
+}
+
+function buildKeywordTree(data) {
+  var adjacencyList = {};
+  var parent;
+  for (i in data) {
+    parent = data[i].parent;
+    if (adjacencyList[parent] == null) {
+      adjacencyList[parent] = [];
+    }
+    adjacencyList[parent].push(data[i]);
+  }
+
+  console.log(adjacencyList);
+}
+
+function constructSearch() {
+  fetchSearchData(function(data) {
+    buildKeywordTree(data);
   });
 }
 
 $(document).ready(function() {
 
-  fetchSearchData();
+  constructSearch();
 });
