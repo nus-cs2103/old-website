12842f5eae474a4338b45135813d224465b0ac56
swxsw <suwei9@gmail.com>
2016-12-12 08:25:33 +0800

    Removed a break from pullContent
    Removed a break from pullContent

--- scripts/common.js
+++ scripts/common.js
@@ -36,7 +36,7 @@ function pullContent(fileName, elementSelector, title, sectionName) {
             $(elementSelector).addClass('embedded');
             $(elementSelector).prepend('<div><div id = "embeddedHeadingContainer"><span class="embeddedHeading">' + title + '</span><button onclick="$(\'' + elementSelector + '\').html(\'\');' +
                ' $(\'' + elementSelector + '\').removeClass(\'embedded\');" ' +
-               'class="btn-dismiss-embedded">X</button></div><br><br> '+linkNotice+' </div>');
+               'class="btn-dismiss-embedded">X</button></div><br> '+linkNotice+' </div>');
             $(elementSelector + ' > div > .btn-dismiss').button();
         }
     }); 
