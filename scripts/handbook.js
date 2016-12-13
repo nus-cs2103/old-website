b0fdc599b0dd82e90d36b0263d018a2b01ec2017
Aaron Chong <aaronjhchong@hotmail.com>
2016-12-13 13:27:16 +0800

    [#117] Cut down loading time of handbook page (#210)
    * Remove forced synchronous behaviour of ajax
    * Remove setTimeout since no synchronous blocking
    * Load handbook sections on demand
    * Jump to section heading in ajax callback
    * Fix anchor offsets
    * Update onclick for Appendix C subsection anchors
    * Update onclick for Appendix B subsection anchors
    * Move contents of Appendix B
    * Update handbook title
    * Leverage load on demand capability for previewing section
    * Move orphan code to document ready function
    * Fix preview for Appendix B policy subsections
    * Load all sections asynchronously if not preview
    * Account for image loading delay

--- scripts/handbook.js
+++ scripts/handbook.js
@@ -1,26 +1,123 @@
-var preview = window.location.href.match(/\?preview=([^&#]*)/);
-if (preview != null) {
-    var section = 'handbook-' + preview[1];
-    sections = [section];
-    $(window).scrollTop($('#' + section).prev().offset().top);
+/**
+ * Jumps to a section's heading.
+ * The header is before the div.
+ */
+function jumpToSectionHeading(section) {
+    var headerPosition = $('#' + section).prev().offset().top;
+    $(window).scrollTop(headerPosition);
 }
 
-setTimeout(function () {
+/**
+ * Adds jumps-to-section-heading behaviour for anchors.
+ * Previous behaviour is remove and default is blocked.
+ */
+function addJumpToSectionHeadingBehavior(anchors) {
+    anchors.off(); // Remove previous behaviour
+    anchors.click(function(event) {
+        event.preventDefault(); // Prevent default behaviour
+        jumpToSectionHeading(this.hash.substring(1));
+    });
+}
+
+/**
+ * Adds load-on-demand capability for anchors in table-of-contents.
+ * This capability is triggered when any of the anchors is clicked.
+ */
+function addLoadOnDemandToAnchors() {
+    var allAnchors = $('a');
+    handleClickForAllAnchors(allAnchors);
+
+    // Appendix B
+    var policySection = 'handbook-appendixB-policies';
+    var policyAnchors = $('a[href^="#policy-"]');
+    handleClickForAppendixAnchors(policySection, policyAnchors);
+
+    // Appendix C
+    var faqSection = 'handbook-appendixC-faq';
+    var faqAnchors = $('a[href="#' + faqSection + '"]').next().find('a');
+    handleClickForAppendixAnchors(faqSection, faqAnchors);
+}
+
+/**
+ * Handles onclick event for all anchors.
+ * For first click, load 'section' using ajax then jump to 'section' heading.
+ * For subsequent clicks, jump to 'section' heading only.
+ */
+function handleClickForAllAnchors(allAnchors) {
+    allAnchors.click(function() {
+        var thisAnchor = $(this);
+        var section = this.hash.substring(1);
+        var callback = function() {
+            jumpToSectionHeading(section);
+            addJumpToSectionHeadingBehavior(thisAnchor);
+        };
+        loadSectionUsingAjax(section, callback);
+    });
+}
+
+/**
+ * Handles onclick event for Appendix B and C anchors (subsections in common HTML file).
+ * For first click, load 'containerSection' using ajax then jump to 'subsection' heading.
+ * For subsequent clicks, jump to 'subsection' heading only.
+ */
+function handleClickForAppendixAnchors(containerSection, subsectionAnchors) {
+    subsectionAnchors.off(); // Remove previous behaviour
+    subsectionAnchors.click(function() {
+        var subsection = this.hash.substring(1);
+        var callback = function() {
+            jumpToSectionHeading(subsection);
+            addJumpToSectionHeadingBehavior(subsectionAnchors);
+        };
+        loadSectionUsingAjax(containerSection, callback);
+    });
+}
+
+/**
+ * Loads a single section on demand using ajax.
+ * On success, execute the callback.
+ */
+function loadSectionUsingAjax(section, callback) {
+    $.ajax({
+        type: 'GET',
+        url: section + '.html',
+        error: function() {
+        },
+        success: function(data) {
+            $('#' + section).html(data);
+            var images = $('#' + section + ' img');
+            if (images.length == 0) {
+                callback();
+            } else {
+                var imagesLoaded = 0;
+                images.on('load', function() {
+                    imagesLoaded++;
+                    if (imagesLoaded == images.length) {
+                        callback();
+                    }
+                });
+            }
+        }
+    });
+}
+
+/**
+ * Load all sections asynchronously.
+ * Increment number of sections loaded in callback.
+ * If all sections loaded, remove the loading overlay.
+ */
+function loadAllSections() {
+    var sectionsLoaded = 0;
     for (var i in sections) {
         var section = sections[i];
-        $.ajax({
-            type: 'GET',
-            async: false,
-            url: section + '.html',
-            error: function() {
-            },
-            success: function(data) {
-                $('#' + section).html(data);
+        var callback = function() {
+            sectionsLoaded++;
+            if (sectionsLoaded == sections.length) {
+                $('#overlay').remove();
             }
-        });
+        };
+        loadSectionUsingAjax(section, callback);
     }
-    $('#overlay').remove();
-}, 0);
+}
 
 function isTableOfContentVisible() {
     var windowTop = $(window).scrollTop();
@@ -30,6 +127,22 @@ function isTableOfContentVisible() {
 }
 
 $(document).ready(function() {
+    var preview = window.location.href.match(/\?preview=([^&#]*)/);
+
+    if (preview) {
+        var section = preview[1];
+        var isNotPolicySection = (section.match(/^policy-/) == null);
+        if (isNotPolicySection) {
+            section = 'handbook-' + section;
+        }
+        addLoadOnDemandToAnchors();
+        $('a[href="#' + section + '"]').click();
+        $('#overlay').remove();
+    } else {
+        loadAllSections();
+        addJumpToSectionHeadingBehavior($('a'));
+    }
+
     var buttonAnimationDuration = 200;
     var speed = 1;
 
@@ -42,12 +155,6 @@ $(document).ready(function() {
     $('#back-to-top-button').button();
     $('#back-to-top-button').click(function(e) {
         scrollToPosition(0);
-    });
-
-    $('a').click(function() {
-        var adjustment = 73;
-        var header = this.hash;
-        scrollToPosition($(header).offset().top - adjustment);
     });
 
     $(window).scroll(function() {
