b1dbdaee9f7a96b79c89b2c5686c26861341f03c
Aaron <aaronjhchong@hotmail.com>
2016-12-15 03:01:07 +0800

    Support HTML content

--- scripts/tooltip.js
+++ scripts/tooltip.js
@@ -1,9 +1,14 @@
 var definitions = {
 };
 
+var count = 0;
 $('tooltip').each(function() {
     var term = $(this).text();
     var title = definitions[term];
-    $(this).attr('title', title);
-    $(this).tooltip();
+    var id = String(count++);
+    $(this).attr('id', id);
+    $(this).tooltip({
+        items: '#' + id,
+        content: title
+    });
 });
