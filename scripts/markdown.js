a1f841e834d02aa40538ec82ae481862be6ffefa
Aaron Chong <aaron.chong13@sps.nus.edu.sg>
2016-12-16 13:23:32 +0800

    Remove prefix added by GitHub markdown API

--- scripts/markdown.js
+++ scripts/markdown.js
@@ -32,9 +32,10 @@ function displayResult(html) {
 
 function processAndDisplayResult(html) {
     // Pre-process the result as necessary
+    html = html.replace(/user-content-/g, '');
     html = html.replace(/<th align="center">Good<\/th>/g, '<th align="center" class="example-good">Good</th>');
     html = html.replace(/<th align="center">Bad<\/th>/g, '<th align="center" class="example-bad">Bad</th>');
-    html = html.replace('id="user-content-borderless"', 'class="borderless"');
+    html = html.replace('id="borderless"', 'class="borderless"');
 
     // Display the result in the page; this is the minimum requirement of the function
     displayResult(html);
